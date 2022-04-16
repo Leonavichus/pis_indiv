a1709ef52cb616a1f8d84ac49f0f47c3691f7eb2
Yoshi <yoshi@localhost.localdomain>
2022-04-16 15:45:19 +0300

    pis-indiv

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=autocraft',
+    'username' => 'root',
+    'password' => '30q04q2019',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
