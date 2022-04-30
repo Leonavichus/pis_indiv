4741570b87ccca60eb7725669519b300440c1974
Yoshi <yoshi@localhost.localdomain>
2022-04-30 09:36:57 +0300

    php-indiv

--- controllers/RequestController.php
+++ controllers/RequestController.php
@@ -6,20 +6,42 @@ use Yii;
 use yii\web\Controller;
 use yii\db\Query;
 
+use app\models\Req1Form;
+use app\models\Req2Form;
+use app\models\Req3Form;
 use app\models\Req4Form;
+use app\models\Req5Form;
+use app\models\Req6Form;
+use app\models\Req7Form;
+use app\models\Req8Form;
+use app\models\Req9Form;
+use app\models\Req10Form;
+use app\models\Req11Form;
+use app\models\Req12Form;
+use app\models\Req13Form;
+use app\models\Req14Form;
 
 class RequestController extends Controller
 {
     public function actionReq1()
     {
+        $model = new Req1Form();
+        $query = (new Query())
+            ->select('');
     }
 
     public function actionReq2()
     {
+        $model = new Req2Form();
+        $query = (new Query())
+            ->select('');
     }
 
     public function actionReq3()
     {
+        $model = new Req3Form();
+        $query = (new Query())
+            ->select('');
     }
 
     public function actionReq4()
@@ -31,41 +53,72 @@ class RequestController extends Controller
 
     public function actionReq5()
     {
+        $model = new Req5Form();
+        $query = (new Query())
+            ->select('');
     }
 
     public function actionReq6()
     {
+        $model = new Req6Form();
+        $query = (new Query())
+            ->select('');
     }
 
     public function actionReq7()
     {
+        $model = new Req7Form();
+        $query = (new Query())
+            ->select('');
     }
 
     public function actionReq8()
     {
+        $model = new Req8Form();
+        $query = (new Query())
+            ->select('');
     }
 
     public function actionReq9()
     {
+        $model = new Req9Form();
+        $query = (new Query())
+            ->select('');
     }
 
     public function actionReq10()
     {
+        $model = new Req10Form();
+        $query = (new Query())
+            ->select('');
     }
 
     public function actionReq11()
     {
+        $model = new Req11Form();
+        $query = (new Query())
+            ->select('');
     }
 
     public function actionReq12()
     {
+        $model = new Req12Form();
+        $query = (new Query())
+            ->select('');
     }
+
 
     public function actionReq13()
     {
+        $model = new Req13Form();
+        $query = (new Query())
+            ->select('');
     }
 
     public function actionReq14()
     {
+        $model = new Req14Form();
+        $query = (new Query())
+            ->select('');
     }
 }
